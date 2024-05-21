6cff39e701c8fcf36ab267e386ed11a8d135597c
Dmitry Dygalo <dmitry@dygalo.dev>
2024-05-21 16:51:27 +0200

    chore: Generate random references
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/hypothesis_openapi/_strategies.py
+++ src/hypothesis_openapi/_strategies.py
@@ -6,5 +6,5 @@ from ._serialize import asdict
 
 def openapis(version: Literal["2.0"] | Literal["3.0"] | Literal["3.1"]) -> "st.SearchStrategy[dict[str, Any]]":
     if version == "2.0":
-        return st.from_type(Swagger).map(asdict)  # type: ignore[arg-type]
+        return st.from_type(Swagger).flatmap(asdict)  # type: ignore[arg-type]
     raise ValueError(f"Unsupported version: {version}")
